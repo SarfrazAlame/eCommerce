3fd1449e0467af8592817b438c992430c7dca084
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-03-03 11:23:28 +0530

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div>
+          <div>
+            <Header />
+          </div>
+          <div>
+            <Hero/>
+          </div>
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="w-full md:px-16 h-16 bg-gray-800 text-white shadow-lg flex items-center">
+      <h1 className="text-2xl">FlipZon</h1>
+    </div>
+  );
+};
+
+export default Header;
